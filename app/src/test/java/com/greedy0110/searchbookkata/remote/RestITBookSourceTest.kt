687e2b9ce19cc51f22e8e8b695083ab7b5883822
greedy0110 <greedy0110@gmail.com>
2021-11-24 14:29:38 +0900

    Pass catchRetrofitErrorAndRethrowRepositoryError

--- app/src/test/java/com/greedy0110/searchbookkata/remote/RestITBookSourceTest.kt
+++ app/src/test/java/com/greedy0110/searchbookkata/remote/RestITBookSourceTest.kt
@@ -2,6 +2,7 @@ package com.greedy0110.searchbookkata.remote
 
 import com.greedy0110.searchbookkata.domain.ITBook
 import com.greedy0110.searchbookkata.mock.DummyITBooks
+import com.greedy0110.searchbookkata.repository.RemoteException
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -48,25 +49,44 @@ class RestITBookSourceTest {
             .assertValue(DummyITBooks.javaPage1.subList(0, 3))
     }
 
+    @Test
+    fun catchRetrofitErrorAndRethrowRepositoryError() {
+        mockWebServer.dispatcher = SearchBooksDispatcher(
+            getITBooks = { throw Exception() }
+        )
+
+        val keyword = "java"
+        val page = 1
+        restITBookSource.getByKeyword(keyword, page)
+            .test()
+            .await()
+            .assertError(RemoteException::class.java)
+    }
+
     private class SearchBooksDispatcher(
         private val getITBooks: () -> List<ITBook>
     ) : Dispatcher() {
         override fun dispatch(request: RecordedRequest): MockResponse {
             return when {
                 request.path?.startsWith("/1.0/search/") == true -> {
-                    val books = getITBooks()
-                        .map { it.toSearchBooksResponseBook() }
-                    val responseObject = SearchBooksResponse(
-                        error = 0,
-                        total = 100,
-                        page = 1,
-                        books = books
-                    )
-                    val adapter = moshi.adapter(SearchBooksResponse::class.java)
-                    val rawResponse = adapter.toJson(responseObject)
-                    MockResponse()
-                        .setResponseCode(HttpURLConnection.HTTP_OK)
-                        .setBody(rawResponse)
+                    try {
+                        val books = getITBooks()
+                            .map { it.toSearchBooksResponseBook() }
+                        val responseObject = SearchBooksResponse(
+                            error = 0,
+                            total = 100,
+                            page = 1,
+                            books = books
+                        )
+                        val adapter = moshi.adapter(SearchBooksResponse::class.java)
+                        val rawResponse = adapter.toJson(responseObject)
+                        MockResponse()
+                            .setResponseCode(HttpURLConnection.HTTP_OK)
+                            .setBody(rawResponse)
+                    } catch (throwable: Throwable) {
+                        MockResponse()
+                            .setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
+                    }
                 }
                 else -> throw UnsupportedOperationException("the request is not allowed on this dispatcher.")
             }
